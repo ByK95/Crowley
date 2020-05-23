a3f5fb1ad06d6545e94c66d21a1bb04cee7027c0
Typic <byrmkaya97@gmail.com>
2020-05-23 13:45:53 +0300

    update database about crawler changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask , render_template , send_from_directory , request , jsonify
-from database import TimerLog, Session, SpiderDB
+from database import TimerLog, Session, SpiderDB , SpiderUrl, SpiderSelector
 from helper import getSpiders , loadSpider , getLastSpiderResult , getCrawlerInfo
 import subprocess
 from sqlalchemy import desc
@@ -56,31 +56,35 @@ def listcrawlers():
 
     return render_template("table.html",tableTitle=title,data=data,headers=headers)
 
-@app.route('/crawler/<int:id>')
-def crawler_info(id):
+@app.route('/crawler/<int:id>', methods=['GET','POST'])
+def crawler_edit(id):
+    if request.method == 'POST':
+        urls = []
+        selectors = []
+
+        index = 1
+        while request.form.get('url{}'.format(index)) != None and index < 16:
+            urls.append(request.form.get('url{}'.format(index)))
+            index += 1
+
+        index = 1
+        while request.form.get('selector{}'.format(index)) != None and index < 16:
+            selectors.append(request.form.get('selector{}'.format(index)))
+            index += 1
+
+        session = Session()
+        session.query(SpiderUrl).filter(SpiderUrl.spider_id == id).delete()
+        session.query(SpiderSelector).filter(SpiderSelector.spider_id == id).delete()
+        for url in urls:
+            session.add(SpiderUrl(spider_id=id, url=url))
+            
+        for selector in selectors:
+            session.add(SpiderSelector(spider_id=id, selector=selector))
+
+        session.commit()
+    
     pairs , urls = getCrawlerInfo(id)
     return render_template("crawler.html",tableTitle="Crawler Name",pairs=pairs,urls=urls,id=id)
-
-@app.route('/crawler/edit/<id>', methods=['POST'])
-def crawler_edit(id):
-    urls = []
-    selectors = []
-
-    index = 1
-    while request.form.get('url{}'.format(index)) != None and index < 16:
-        urls.append(request.form.get('url{}'.format(index)))
-        index += 1
-
-    index = 1
-    while request.form.get('selector{}'.format(index)) != None and index < 16:
-        selectors.append(request.form.get('selector{}'.format(index)))
-        index += 1
-
-    print(urls)
-    print('')
-    print(selectors)
-    return 'OK'
-
 
 @app.route('/api/log/timer' , methods=['POST'])
 def log_timer_usage():
